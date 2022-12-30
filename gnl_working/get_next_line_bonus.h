c2b12247a4bcdff1e5e006757d9c750fc3a37a34
Drew Pentland <drewtpentland@gmail.com>
2022-12-30 18:02:35 +0100

    Forgot to run norm check. Good now.

--- gnl_working/get_next_line_bonus.h
+++ gnl_working/get_next_line_bonus.h
@@ -1,17 +1,17 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dpentlan <dpentlan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/01 13:53:29 by dpentlan          #+#    #+#             */
-/*   Updated: 2022/12/01 13:53:32 by dpentlan         ###   ########.fr       */
+/*   Updated: 2022/12/30 18:02:06 by dpentlan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef GET_NEXT_LINE_H
-# define GET_NEXT_LINE_H
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 1024
 # endif
